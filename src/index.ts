f8690db97677dca8ba25b597f8d5ef76d0947be9
Edu-Maturana <edu.maturana.c@gmail.com>
2025-10-29 21:16:20 -0300

    feat: implement portfolio management with target allocation and rebalance functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,57 @@
 import Stock from "./stock/stock";
+import Portfolio from "./portfolio/portfolio";
+
+// Voy a simularlo con lo mismo que tengo en Fintual
+const myStocks = [
+  {
+    symbol: "VOO",
+    name: "Vanguard S&P 500 ETF",
+    shares: 5.248567795,
+    price: 631.93,
+  },
+  {
+    symbol: "BTC",
+    name: "Bitcoin",
+    shares: 0.01392148,
+    price: 110748.67,
+  },
+  {
+    symbol: "IAUM",
+    name: "IShares Gold Trust Micro",
+    shares: 14.444292189,
+    price: 39.32,
+  },
+];
+
+// Y acá mi distribución objetivo
+const targetAllocation = [
+  { symbol: "VOO", percentage: 60 },
+  { symbol: "BTC", percentage: 30 },
+  { symbol: "IAUM", percentage: 10 },
+];
 
 async function main() {
-  const stock = new Stock("MCD", "McDonald's Corporation", 100);
-  const price = await stock.getCurrentPrice();
-  console.log(`El precio de ${stock.name} (${stock.symbol}) es: ${price}`);
+  const stocks = myStocks.map(
+    (stock) => new Stock(stock.symbol, stock.name, stock.shares, stock.price)
+  );
+  const portfolio = new Portfolio(stocks, targetAllocation);
+
+  console.log(
+    `Estado actual del portafolio:\n${portfolio.getCurrentPortfolioStatus()}\n`
+  );
+
+  console.log(
+    `Plan de rebalanceo:\n${portfolio
+      .getRebalancePlan()
+      .split("\n")
+      .join("\n")}\n`
+  );
+
+  portfolio.rebalancePortfolio();
+
+  console.log(
+    `Estado del portafolio después de rebalancear:\n${portfolio.getCurrentPortfolioStatus()}`
+  );
 }
 
 main().catch((error) => {
